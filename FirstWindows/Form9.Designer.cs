5c13053743208b8613f3fdd070f3d53c665bee45
prashantthite.pdt@gmail.com <prashantthite.pdt@gmail.com>
2022-05-07 18:11:13 +0530

    Created form to store Batch details by using binarywriter and reader

---
+++ FirstWindows/Form9.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace FirstWindows
+{
+    partial class Form9
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtEndDate = new System.Windows.Forms.TextBox();
+            this.txtstartdate = new System.Windows.Forms.TextBox();
+            this.txtBatchName = new System.Windows.Forms.TextBox();
+            this.txtBatchId = new System.Windows.Forms.TextBox();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.txtTrainer = new System.Windows.Forms.TextBox();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(255, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Batch Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(255, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Batch Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(255, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Start date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(255, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "End date";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(255, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Location";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(255, 210);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Trainer Name";
+            // 
+            // txtEndDate
+            // 
+            this.txtEndDate.Location = new System.Drawing.Point(342, 141);
+            this.txtEndDate.Name = "txtEndDate";
+            this.txtEndDate.Size = new System.Drawing.Size(100, 20);
+            this.txtEndDate.TabIndex = 6;
+            // 
+            // txtstartdate
+            // 
+            this.txtstartdate.Location = new System.Drawing.Point(342, 109);
+            this.txtstartdate.Name = "txtstartdate";
+            this.txtstartdate.Size = new System.Drawing.Size(100, 20);
+            this.txtstartdate.TabIndex = 7;
+            // 
+            // txtBatchName
+            // 
+            this.txtBatchName.Location = new System.Drawing.Point(342, 72);
+            this.txtBatchName.Name = "txtBatchName";
+            this.txtBatchName.Size = new System.Drawing.Size(100, 20);
+            this.txtBatchName.TabIndex = 8;
+            // 
+            // txtBatchId
+            // 
+            this.txtBatchId.Location = new System.Drawing.Point(342, 38);
+            this.txtBatchId.Name = "txtBatchId";
+            this.txtBatchId.Size = new System.Drawing.Size(100, 20);
+            this.txtBatchId.TabIndex = 9;
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(342, 177);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(100, 20);
+            this.txtLocation.TabIndex = 10;
+            // 
+            // txtTrainer
+            // 
+            this.txtTrainer.Location = new System.Drawing.Point(342, 207);
+            this.txtTrainer.Name = "txtTrainer";
+            this.txtTrainer.Size = new System.Drawing.Size(100, 20);
+            this.txtTrainer.TabIndex = 11;
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(215, 269);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 12;
+            this.btnWrite.Text = "Write to file";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(342, 269);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Read file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(475, 269);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(75, 23);
+            this.btnclear.TabIndex = 14;
+            this.btnclear.Text = "clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(326, 109);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(20, 20);
+            this.dateTimePicker1.TabIndex = 15;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(326, 141);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(18, 20);
+            this.dateTimePicker2.TabIndex = 16;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // Form9
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.txtTrainer);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.txtBatchId);
+            this.Controls.Add(this.txtBatchName);
+            this.Controls.Add(this.txtstartdate);
+            this.Controls.Add(this.txtEndDate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form9";
+            this.Text = "Batch Details";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtEndDate;
+        private System.Windows.Forms.TextBox txtstartdate;
+        private System.Windows.Forms.TextBox txtBatchName;
+        private System.Windows.Forms.TextBox txtBatchId;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.TextBox txtTrainer;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+    }
+}
---
+++ FirstWindows/Form9.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace FirstWindows
+{
+    public partial class Form9 : Form
+    {
+        FileStream fs;
+        public Form9()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            txtstartdate.Text = dateTimePicker1.Text;
+        }
+             
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            txtEndDate.Text = dateTimePicker2.Text;
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            txtBatchId.Clear();
+            txtBatchName.Clear();
+            txtEndDate.Clear();
+            txtLocation.Clear();
+            txtstartdate.Clear();
+            txtTrainer.Clear();
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                fs = new FileStream(@"D:\Test Folder\Batch Details.txt", FileMode.Create, FileAccess.Write);
+                BinaryWriter br = new BinaryWriter(fs);
+                br.Write(Convert.ToInt32(txtBatchId.Text));
+                br.Write(txtBatchName.Text);
+                br.Write(txtstartdate.Text);
+                br.Write(txtEndDate.Text);
+                br.Write(txtLocation.Text);
+                br.Write(txtTrainer.Text);
+                br.Close();
+                MessageBox.Show("Done");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                fs = new FileStream(@"D:\Test Folder\Batch Details.txt", FileMode.Open, FileAccess.Read);
+                BinaryReader br = new BinaryReader(fs);
+                txtBatchId.Text = br.ReadInt32().ToString();
+                txtBatchName.Text = br.ReadString();
+                txtstartdate.Text = br.ReadString();
+                txtEndDate.Text = br.ReadString();
+                txtLocation.Text = br.ReadString();
+                txtTrainer.Text = br.ReadString();
+                br.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+    }
+}
