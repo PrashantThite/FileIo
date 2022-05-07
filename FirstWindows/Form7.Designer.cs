34daff4c0d1dc8833d3bb68978358a4f2205b253
prashantthite.pdt@gmail.com <prashantthite.pdt@gmail.com>
2022-05-07 16:31:04 +0530

    Created new form of product detal and used serializestion

---
+++ FirstWindows/Form7.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace FirstWindows
+{
+    partial class Form7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lcategory = new System.Windows.Forms.Label();
+            this.Lprice = new System.Windows.Forms.Label();
+            this.LID = new System.Windows.Forms.Label();
+            this.Lname = new System.Windows.Forms.Label();
+            this.txtcategory = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnJsonRead = new System.Windows.Forms.Button();
+            this.btnJsonWrite = new System.Windows.Forms.Button();
+            this.btnSoapRead = new System.Windows.Forms.Button();
+            this.BtnSoapWrite = new System.Windows.Forms.Button();
+            this.btnXmlRead = new System.Windows.Forms.Button();
+            this.btnXmlWrite = new System.Windows.Forms.Button();
+            this.btnBinaryRead = new System.Windows.Forms.Button();
+            this.btnBinaryWrite = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Lcategory
+            // 
+            this.Lcategory.AutoSize = true;
+            this.Lcategory.Location = new System.Drawing.Point(298, 201);
+            this.Lcategory.Name = "Lcategory";
+            this.Lcategory.Size = new System.Drawing.Size(49, 13);
+            this.Lcategory.TabIndex = 0;
+            this.Lcategory.Text = "Category";
+            // 
+            // Lprice
+            // 
+            this.Lprice.AutoSize = true;
+            this.Lprice.Location = new System.Drawing.Point(298, 161);
+            this.Lprice.Name = "Lprice";
+            this.Lprice.Size = new System.Drawing.Size(31, 13);
+            this.Lprice.TabIndex = 1;
+            this.Lprice.Text = "Price";
+            // 
+            // LID
+            // 
+            this.LID.AutoSize = true;
+            this.LID.Location = new System.Drawing.Point(298, 117);
+            this.LID.Name = "LID";
+            this.LID.Size = new System.Drawing.Size(16, 13);
+            this.LID.TabIndex = 2;
+            this.LID.Text = "Id";
+            // 
+            // Lname
+            // 
+            this.Lname.AutoSize = true;
+            this.Lname.Location = new System.Drawing.Point(298, 72);
+            this.Lname.Name = "Lname";
+            this.Lname.Size = new System.Drawing.Size(35, 13);
+            this.Lname.TabIndex = 3;
+            this.Lname.Text = "Name";
+            // 
+            // txtcategory
+            // 
+            this.txtcategory.Location = new System.Drawing.Point(398, 201);
+            this.txtcategory.Name = "txtcategory";
+            this.txtcategory.Size = new System.Drawing.Size(161, 20);
+            this.txtcategory.TabIndex = 4;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(398, 161);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(161, 20);
+            this.txtPrice.TabIndex = 5;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(398, 110);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(161, 20);
+            this.txtId.TabIndex = 6;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(398, 65);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(161, 20);
+            this.txtName.TabIndex = 7;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(367, 284);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(106, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnJsonRead
+            // 
+            this.btnJsonRead.Location = new System.Drawing.Point(629, 399);
+            this.btnJsonRead.Name = "btnJsonRead";
+            this.btnJsonRead.Size = new System.Drawing.Size(75, 23);
+            this.btnJsonRead.TabIndex = 9;
+            this.btnJsonRead.Text = "Json Read";
+            this.btnJsonRead.UseVisualStyleBackColor = true;
+            this.btnJsonRead.Click += new System.EventHandler(this.btnJsonRead_Click);
+            // 
+            // btnJsonWrite
+            // 
+            this.btnJsonWrite.Location = new System.Drawing.Point(629, 361);
+            this.btnJsonWrite.Name = "btnJsonWrite";
+            this.btnJsonWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnJsonWrite.TabIndex = 10;
+            this.btnJsonWrite.Text = "Json Write";
+            this.btnJsonWrite.UseVisualStyleBackColor = true;
+            this.btnJsonWrite.Click += new System.EventHandler(this.btnJsonWrite_Click);
+            // 
+            // btnSoapRead
+            // 
+            this.btnSoapRead.Location = new System.Drawing.Point(629, 284);
+            this.btnSoapRead.Name = "btnSoapRead";
+            this.btnSoapRead.Size = new System.Drawing.Size(75, 23);
+            this.btnSoapRead.TabIndex = 11;
+            this.btnSoapRead.Text = "Soap Read";
+            this.btnSoapRead.UseVisualStyleBackColor = true;
+            this.btnSoapRead.Click += new System.EventHandler(this.btnSoapRead_Click);
+            // 
+            // BtnSoapWrite
+            // 
+            this.BtnSoapWrite.Location = new System.Drawing.Point(629, 243);
+            this.BtnSoapWrite.Name = "BtnSoapWrite";
+            this.BtnSoapWrite.Size = new System.Drawing.Size(75, 23);
+            this.BtnSoapWrite.TabIndex = 12;
+            this.BtnSoapWrite.Text = "Soap Write";
+            this.BtnSoapWrite.UseVisualStyleBackColor = true;
+            this.BtnSoapWrite.Click += new System.EventHandler(this.BtnSoapWrite_Click);
+            // 
+            // btnXmlRead
+            // 
+            this.btnXmlRead.Location = new System.Drawing.Point(629, 174);
+            this.btnXmlRead.Name = "btnXmlRead";
+            this.btnXmlRead.Size = new System.Drawing.Size(75, 23);
+            this.btnXmlRead.TabIndex = 13;
+            this.btnXmlRead.Text = "Xml Read";
+            this.btnXmlRead.UseVisualStyleBackColor = true;
+            this.btnXmlRead.Click += new System.EventHandler(this.btnXmlRead_Click);
+            // 
+            // btnXmlWrite
+            // 
+            this.btnXmlWrite.Location = new System.Drawing.Point(629, 135);
+            this.btnXmlWrite.Name = "btnXmlWrite";
+            this.btnXmlWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnXmlWrite.TabIndex = 14;
+            this.btnXmlWrite.Text = "Xml Write";
+            this.btnXmlWrite.UseVisualStyleBackColor = true;
+            this.btnXmlWrite.Click += new System.EventHandler(this.btnXmlWrite_Click);
+            // 
+            // btnBinaryRead
+            // 
+            this.btnBinaryRead.Location = new System.Drawing.Point(629, 63);
+            this.btnBinaryRead.Name = "btnBinaryRead";
+            this.btnBinaryRead.Size = new System.Drawing.Size(75, 23);
+            this.btnBinaryRead.TabIndex = 15;
+            this.btnBinaryRead.Text = "Binary Read";
+            this.btnBinaryRead.UseVisualStyleBackColor = true;
+            this.btnBinaryRead.Click += new System.EventHandler(this.btnBinaryRead_Click);
+            // 
+            // btnBinaryWrite
+            // 
+            this.btnBinaryWrite.Location = new System.Drawing.Point(629, 24);
+            this.btnBinaryWrite.Name = "btnBinaryWrite";
+            this.btnBinaryWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnBinaryWrite.TabIndex = 16;
+            this.btnBinaryWrite.Text = "Binary Write";
+            this.btnBinaryWrite.UseVisualStyleBackColor = true;
+            this.btnBinaryWrite.Click += new System.EventHandler(this.btnBinaryWrite_Click);
+            // 
+            // Form7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBinaryWrite);
+            this.Controls.Add(this.btnBinaryRead);
+            this.Controls.Add(this.btnXmlWrite);
+            this.Controls.Add(this.btnXmlRead);
+            this.Controls.Add(this.BtnSoapWrite);
+            this.Controls.Add(this.btnSoapRead);
+            this.Controls.Add(this.btnJsonWrite);
+            this.Controls.Add(this.btnJsonRead);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtcategory);
+            this.Controls.Add(this.Lname);
+            this.Controls.Add(this.LID);
+            this.Controls.Add(this.Lprice);
+            this.Controls.Add(this.Lcategory);
+            this.Name = "Form7";
+            this.Text = "Product Details";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lcategory;
+        private System.Windows.Forms.Label Lprice;
+        private System.Windows.Forms.Label LID;
+        private System.Windows.Forms.Label Lname;
+        private System.Windows.Forms.TextBox txtcategory;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnJsonRead;
+        private System.Windows.Forms.Button btnJsonWrite;
+        private System.Windows.Forms.Button btnSoapRead;
+        private System.Windows.Forms.Button BtnSoapWrite;
+        private System.Windows.Forms.Button btnXmlRead;
+        private System.Windows.Forms.Button btnXmlWrite;
+        private System.Windows.Forms.Button btnBinaryRead;
+        private System.Windows.Forms.Button btnBinaryWrite;
+    }
+}
---
+++ FirstWindows/Form7.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Text.Json;
+
+namespace FirstWindows
+{
+
+    public partial class Form7 : Form
+    {
+        FileStream fs;
+        public Form7()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtcategory.Clear();
+            txtId.Clear();
+            txtName.Clear();
+            txtPrice.Clear();
+        }
+
+        private void btnBinaryWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Product prod = new Product();
+                fs = new FileStream(@"D:\Test Folder\Product Binary.txt", FileMode.Create, FileAccess.Write);
+                prod.Name = txtName.Text;
+                prod.Id = Convert.ToInt32(txtId.Text);
+                prod.Price = Convert.ToInt32(txtPrice.Text);
+                prod.Category = txtcategory.Text;
+                BinaryFormatter binary = new BinaryFormatter();
+                binary.Serialize(fs, prod);
+                MessageBox.Show("Done");
+
+            }
+            catch ( Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+        private void btnBinaryRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Product prod = new Product();
+                fs = new FileStream(@"D:\Test Folder\Product Binary.txt", FileMode.Open, FileAccess.Read);
+                BinaryFormatter binary = new BinaryFormatter();
+               prod= (Product)binary.Deserialize(fs);
+                txtName.Text = prod.Name;
+                txtId.Text = prod.Id.ToString();
+                txtPrice.Text = prod.Price.ToString();
+                txtcategory.Text = prod.Category;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+        private void btnXmlWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Product prod = new Product();
+                prod.Category = txtcategory.Text;
+                prod.Id = Convert.ToInt32(txtId.Text);
+                prod.Name = txtName.Text;
+                prod.Price = Convert.ToInt32(txtPrice.Text);
+                fs = new FileStream(@"D:\Test Folder\Product Xml.txt", FileMode.Create, FileAccess.Write);
+                XmlSerializer xml = new XmlSerializer(typeof(Product));
+                xml.Serialize(fs, prod);
+                MessageBox.Show("done");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+        private void btnXmlRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Product prod = new Product();
+                fs = new FileStream(@"D:\Test Folder\Product Xml.txt", FileMode.Open, FileAccess.Read);
+                XmlSerializer xml = new XmlSerializer(typeof(Product));
+                prod = (Product)xml.Deserialize(fs);
+                txtName.Text = prod.Name;
+                txtId.Text = prod.Id.ToString();
+                txtPrice.Text = prod.Price.ToString();
+                txtcategory.Text = prod.Category;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+             finally
+            {
+                fs.Close();
+            }
+
+        }
+
+        private void BtnSoapWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Product prod = new Product();
+                prod.Category = txtcategory.Text;
+                prod.Id = Convert.ToInt32(txtId.Text);
+                prod.Name = txtName.Text;
+                prod.Price = Convert.ToInt32(txtPrice.Text);
+                fs = new FileStream(@"D:\Test Folder\Product soap.txt", FileMode.Create, FileAccess.Write);
+                SoapFormatter soap = new SoapFormatter();
+                soap.Serialize(fs, prod);
+                MessageBox.Show("Done");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+        private void btnSoapRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Product prod = new Product();               
+                fs = new FileStream(@"D:\Test Folder\Product soap.txt", FileMode.Open, FileAccess.Read);
+                SoapFormatter soap = new SoapFormatter();
+              prod=(Product)soap.Deserialize(fs);
+                txtName.Text = prod.Name;
+                txtId.Text = prod.Id.ToString();
+                txtPrice.Text = prod.Price.ToString();
+                txtcategory.Text = prod.Category;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+        private void btnJsonWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Product prod = new Product();
+                prod.Category = txtcategory.Text;
+                prod.Id = Convert.ToInt32(txtId.Text);
+                prod.Name = txtName.Text;
+                prod.Price = Convert.ToInt32(txtPrice.Text);
+                fs = new FileStream(@"D:\Test Folder\Product Json.txt", FileMode.Create, FileAccess.Write);
+                JsonSerializer.Serialize(fs, prod);
+                MessageBox.Show("Done");
+            }
+            catch(Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+        private void btnJsonRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Product prod = new Product();
+                fs = new FileStream(@"D:\Test Folder\Product Json.txt", FileMode.Open, FileAccess.Read);
+               prod= JsonSerializer.Deserialize<Product>(fs);
+                txtName.Text = prod.Name;
+                txtId.Text = prod.Id.ToString();
+                txtPrice.Text = prod.Price.ToString();
+                txtcategory.Text = prod.Category;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+    }
+}
